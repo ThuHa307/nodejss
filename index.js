395a4e8f90807dd785ffa997c44575444e3e33bb
ThuHa307 <hatran3072003@gmail.com>
2022-12-07 21:27:37 +0700

    #2 insert and update data mongodb

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ app.get('/', (req, res) => {
     res.send('Home');
 });
 
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT}`));
--- src/app/controllers/users.js
+++ src/app/controllers/users.js
@@ -1,6 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
 import User from '../model/users.js';
-let users = [];
 
 export const getUsers = (req, res) => {
     User.find({}, function (err, users) {
@@ -12,37 +10,40 @@ export const getUsers = (req, res) => {
     });
 };
 
-export const createUser = (req, res) => {
-    const user = req.body;
-    users.push({ id: uuidv4(), ...user });
-
-    res.send('Successfully created!');
+export const createUser = async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        const saveUser = await newUser.save();
+        res.status(200).json(saveUser);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
 };
 
-export const getUser = (req, res) => {
-    const { id } = req.params;
-    // const foundUser = users.find((user) => user.id === id);
-    User.findById(id, function (err, foundUser) {
-        if (!err) {
-            res.send(foundUser);
-        } else {
-            res.status(404).json({ error: err });
-        }
-    });
+export const getUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
 };
 
-export const deleteUser = (req, res) => {
-    const { id } = req.params;
-    users = users.filter((user) => user.id !== id);
-    res.send(`User with id "${id}" deleted successfully`);
+export const deleteUser = async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.send(`User with id ${req.params.id} has been deleted!`);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
 };
 
-export const updateUser = (req, res) => {
-    const { id } = req.params;
-    const { name, age, book } = req.body;
-    const user = users.find((user) => user.id === id);
-    if (name) user.name = name;
-    if (age) user.age = age;
-    if (book) user.book = book;
-    res.send(`User with id "${id}" has been updated successfully!`);
+export const updateUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        await user.updateOne({ $set: req.body });
+        res.send(`User with id "${req.params.id}" has been updated successfully!`);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
 };
